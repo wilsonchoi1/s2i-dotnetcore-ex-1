b942fe326b3d58494b590e9629cbfe7f30eadd2a
wilsonchoi1 <eewai97@hotmail.com>
2018-06-05 17:44:29 +0800

    Update Program.cs

--- app/Program.cs
+++ app/Program.cs
@@ -16,7 +16,7 @@ namespace app
         {
             BuildWebHost(args).Run();
         }
-
+//test
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
